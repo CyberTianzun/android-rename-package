39fc623c2204378f8c66115417305ad12e1c1406
Hiro <cnzx219@qq.com>
2015-07-17 13:29:42 +0800

    add comment and parseAndroidManifest and parseJava

--- utils/renamehandler.js
+++ utils/renamehandler.js
@@ -12,6 +12,8 @@ var RenameHandler = function (dir, encoding) {
     }
 }
 
+// parse package name from android manifest file
+// then set the value to `this.oldPackageName`
 RenameHandler.prototype.parsePackageName = function (filename) {
     var self = this
     var filepath = path.join(this.dir, filename)
@@ -29,6 +31,8 @@ RenameHandler.prototype.parsePackageName = function (filename) {
     parser.write(fs.readFileSync(filepath))
 }
 
+// parse resource to list all the modify point about custom xmlns defines
+// almost it's a layout resource file
 RenameHandler.prototype.parseResource = function (filename) {
     // cache file to memory
     var filepath = path.join(this.dir, filename)
@@ -48,6 +52,7 @@ RenameHandler.prototype.parseResource = function (filename) {
     parser.write(content)
 }
 
+// parse android manifest to list all the modify point about authorities of provider and action
 RenameHandler.prototype.parseAndroidManifest = function (filename) {
     // cache file to memory
     var filepath = path.join(this.dir, filename)
@@ -57,8 +62,19 @@ RenameHandler.prototype.parseAndroidManifest = function (filename) {
     var parser = new expat.Parser(this.encoding)
     parser.on('startElement', function (name, attrs) {
         for(var attrName in attrs) {
-            if (attrName.match(/^xmlns:/i)) {
-                console.log('found xml define: ' + attrName + " => " + attrs[attrName])
+            if (attrName.match(/provider/i)) {
+                if (attrs['android:authorities'] !== undefined) {
+                    console.log('found provider authorities need to modify => ' + attrs['android:authorities'])
+                } else {
+                    console.log('found provider need to add a special authorities => ' + attrs['android:name'])
+                }
+            } else if (attrName.match(/action/i)) {
+                if (attrs['android:name'] !== undefined) {
+                    // this action is not a system action
+                    if (attrs['android:name'].match(/^com\.android\.intent\.action\./)) {
+                        console.log('found action need to change => ' + attrs['android:name'])
+                    }
+                }
             }
         }
     })
@@ -67,8 +83,25 @@ RenameHandler.prototype.parseAndroidManifest = function (filename) {
     parser.write(content)
 }
 
+// parse java file to list all the modify point about import line which is .R and .BuildConfig
 RenameHandler.prototype.parseJava = function(filename) {
+    // cache file to memory
+    var filepath = path.join(this.dir, filename)
+    var content = fs.readFileSync(filepath)
+    var lines = content.split('\n')
 
+    // read each line
+    for(var i = 0; i < lines.count; i++) {
+        // filter import define line
+        var importClass = lines[i].match(/^\s*import (.*);/)
+        if (importClass) {
+            if (importClass[1].match(this.oldPackageName + '.R')) {
+                console.log('found import line need to change => ' + this.oldPackageName + '.R')
+            } else if (importClass[1].match(this.oldPackageName + '.BuildConfig')) {
+                console.log('found import line need to change => ' + this.oldPackageName + '.BuildConfig')
+            }
+        }
+    }
 }
 
 module.exports = RenameHandler
